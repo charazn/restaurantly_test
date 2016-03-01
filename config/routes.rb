cb4d1a343517fc893c9f62239cecc30a3579689c
Char <charazn37@gmail.com>
2016-03-01 19:26:35 +0800

    Add Devise and authentications views. Edit config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: "restaurants#index"
 
+  # get "/auth/:provider/callback", to: "sessions#create"
+  # get "/auth/failure", to: redirect("/")
+  # get "/signout", to: "sessions#destroy", as: "signout"
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks", registrations: "registrations" } #, path_names: {sign_in: "login"}
+
   resources :restaurants
-  devise_for :users
+  resources :authentications, only: [:index, :show, :destroy]
 end
