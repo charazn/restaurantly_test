1e76fc1a50b757ca9796e0b47825439dc69f6188
Char <charazn37@gmail.com>
2016-03-03 15:33:12 +0800

    Add expires_at to authentications table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229014920) do
+ActiveRecord::Schema.define(version: 20160303064506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160229014920) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expires_at"
   end
 
   create_table "restaurants", force: :cascade do |t|
